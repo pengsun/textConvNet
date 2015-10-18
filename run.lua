f6956929551327e255469e77c18d53ef119140bb
peng sun <pengsun000@gmail.com>
2015-10-18 16:14:18 -0500

    minor

--- run.lua
+++ run.lua
@@ -5,13 +5,13 @@ require 'sys'
 --[[ global options ]]--
 opt = opt or {
   nThread = 2,
-  logPath = 'log/imdb_full_C256', -- output path for log files
+  logPath = 'log/imdb_full_C128', -- output path for log files
   dataSize = 'full',
   epMax = 100,  -- max epoches
   teFreq = 5, -- test every teFreq epoches
   isCuda = true,
   gpuInd = 1, -- gpu #
-  C = 256,   -- #channels
+  C = 128,   -- #channels
   V = 30000, -- #vocabulary
 }
 print('[global options]')
