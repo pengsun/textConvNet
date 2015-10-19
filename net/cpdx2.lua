cfa27a4c4b73a47918a3f11442831528286b04cc
peng sun <pengsun000@gmail.com>
2015-10-19 15:47:44 -0500

    add net/cpdx2.lua

--- net/cpdx2.lua
+++ net/cpdx2.lua
@@ -8,16 +8,19 @@
 --  a0:   M
 --                  lookup table: V, C
 --  a0':  M, C
+--                  dropout
 --------------------------------------------
 --                  conv: C, 256, 3
 --  a1:   M', 256
 --                  pool: 2, 2
 --  a1':  M'', 256
+--                  dropout
 --------------------------------------------
 --                  conv: 256, 256, 3
 --  a2:   M''',256
 --                  Max: dim 1 
 --  a2':  1, 256
+--                  dropout
 --------------------------------------------
 --                  Reshape: 256
 --  a3:   256
@@ -37,13 +40,15 @@ local C = opt.C
 local md = nn:Sequential()
 -- word2vec Layer
 md:add(nn.LookupTable(V, C)) -- (vocabulary size, #channeds)
+md:add(nn.Dropout(0.5))
 -- ConvPool Layer I
 md:add(nn.TemporalConvolution(C, 256, 3))
-md:add(nn.ReLU())
+md:add(nn.ReLU(true))
 md:add(nn.TemporalMaxPooling(2, 2))
+md:add(nn.Dropout(0.5))
 -- ConvPool Layer II
 md:add(nn.TemporalConvolution(256, 256, 3))
-md:add(nn.ReLU())
+md:add(nn.ReLU(true))
 md:add(nn.Max(1))
 md:add(nn.Dropout(0.5))
 -- Output Layer
@@ -51,6 +56,23 @@ md:add(nn.Reshape(256))
 md:add(nn.Linear(256, 2)) -- binary classification
 md:add(nn.LogSoftMax())
 md:float()
+
+--[[ weight initialization ]]--
+local initConv = function()
+  for k, v in pairs(md:findModules('nn.TemporalConvolution')) do
+    local n = v.kW * v.outputFrameSize
+    v.weight:normal(0, math.sqrt(2/n))
+    v.bias:zero()
+  end
+end
+local initLookup = function()
+  for k, v in pairs(md:findModules('nn.LookupTable')) do
+    local n = opt.C
+    v.weight:normal(0, math.sqrt(2/n))
+  end
+end
+initConv()
+initLookup()
 
 --[[ loss ]]--
 local loss = nn.ClassNLLCriterion()
